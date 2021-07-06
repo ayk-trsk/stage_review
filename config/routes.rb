861130ce52d9b164929a3421357f147549e0d37f
ayk-trsk <1219noppoppo@gmail.com>
2021-07-06 08:11:51 +0000

    [Add] deviseを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'public/users/sessions',
+    registrations: 'public/users/registrations',
+  }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
+  #以下管理者のルーティング
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+  }
+
+  namespace :admin do
+  end
 end
