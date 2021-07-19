8c502dc2da391582d70a11f0f6f34c32952169af
ayk-trsk <1219noppoppo@gmail.com>
2021-07-19 07:24:14 +0000

    [Add] 初期データを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: 'admin@example.com',
+  encrypted_password: 'admins',
+)
+
+Genre.create!(
+  name: 'ストレートプレイ',
+)
+
+Genre.create!(
+  name: 'ミュージカル',
+)
+
+Genre.create!(
+  name: 'お笑い',
+)
+
+Genre.create!(
+  name: '2.5次元舞台',
+)
+
+Genre.create!(
+  name: '落語・寄席',
+)
+
+Genre.create!(
+  name: '伝統芸能',
+)
+
+Genre.create!(
+  name: 'ダンス・パフォーマンス',
+)
