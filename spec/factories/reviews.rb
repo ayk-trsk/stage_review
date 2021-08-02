fc0974b6eeef1d929dffeac212a1805b51589126
ayk-trsk <1219noppoppo@gmail.com>
2021-08-02 16:19:46 +0000

    [Clean] コード修正

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :review do
     rate { 1 }
     body { Faker::Lorem.characters(number: 20) }
-    date {'2021/07/01'}
+    date { '2021/07/01' }
     place { Faker::Lorem.characters(number: 10) }
     association :user
     association :stage
--- spec/factories/stages.rb
+++ spec/factories/stages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :stage do
     name { Faker::Lorem.characters(number: 10) }
     genre_id { FactoryBot.create(:genre).id }
-    start_date {'2021/07/01'}
-    end_date {'2021/07/31'}
+    start_date { '2021/07/01' }
+    end_date { '2021/07/31' }
   end
 end
