74fe4a7322bc70ac86f9727d72e766e680bfd6ac
ayk-trsk <1219noppoppo@gmail.com>
2021-08-06 04:56:07 +0000

    [Fix]公演開始日＞公演終了日で登録できてしまうバグを修正

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,10 +1,14 @@
 class Stage < ApplicationRecord
   belongs_to :genre
   has_many :reviews, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :genre, :start_date, :end_date, presence: true
+  validate :end_date_check
 
-  has_many :bookmarks, dependent: :destroy
+  def end_date_check
+    errors.add(:end_date, "は公演開始日と同じ日または遅い日を選択してください") if self.start_date >= self.end_date
+  end
 
   def bookmarked_by?(user)
     bookmarks.where(user_id: user).exists?
